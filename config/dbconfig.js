cbf74212a699130d2529e3c0fc86b6ae9f418d39
zjaym1703 <jym971024@gmail.com>
2023-03-21 20:39:55 +0900

    [#7] dbconfig.js log 제거

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -3,11 +3,6 @@ const mysql = require('mysql2/promise');
 
 dotenv.config()
 
-console.log("HOST : "+process.env.DB_HOST)
-console.log("HOST : "+process.env.DB_PORT)
-console.log("HOST : "+process.env.DB_USER)
-console.log("HOST : "+process.env.DB_PASS)
-console.log("HOST : "+process.env.DB_NAME)
 //MySQL 연결
 module.exports={
     pool : mysql.createPool({
