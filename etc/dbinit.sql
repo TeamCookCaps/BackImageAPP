5b204fe3a30e7742370fafe10e56394a11f153cf
chaejin <paster12@naver.com>
2023-03-30 23:59:16 +0900

    [#17]parent_category 수정, palette 추가

--- etc/dbinit.sql
+++ etc/dbinit.sql
@@ -36,11 +36,12 @@ foreign key(category_name) references CategoryInfo(category_name),
 foreign key(image_id) references ImageInfo(id)
 );
 
-create table parent_category(
+create table Parent_category(
     category_name varchar(20) not null,
     parent_name varchar(20) not null,
 primary key(category_name),
-foreign key(category_name) references CategoryInfo(category_name)
+foreign key(category_name) references CategoryInfo(category_name),
+foreign key(parent_name) references CategoryInfo(category_name)
 );
 
 create table StoryMusic(
@@ -68,6 +69,14 @@ foreign key(id) references StoryInfo(id),
 foreign key(imageId) references ImageInfo(id)
 );
 
+create table Palette(
+    image_id int not null,
+    rgb varchar(20) not null,
+    rgb_type varchar(8) not null,
+primary key(image_id),
+foreign key(image_id) references ImageInfo(id)
+);
+
 create table TestTable(
 	testId int,
     testPw int,
@@ -75,3 +84,46 @@ primary key(testId)
 );
 
 insert into TestTable values (1, 100);
+
+insert into CategoryInfo(category_name) values ('animal');
+insert into CategoryInfo(category_name) values ('dog');
+insert into CategoryInfo(category_name) values ('cat');
+insert into CategoryInfo(category_name) values ('fish');
+insert into CategoryInfo(category_name) values ('hamster');
+insert into CategoryInfo(category_name) values ('plant');
+insert into CategoryInfo(category_name) values ('leaf');
+insert into CategoryInfo(category_name) values ('flower');
+insert into CategoryInfo(category_name) values ('tree');
+insert into CategoryInfo(category_name) values ('fashion');
+insert into CategoryInfo(category_name) values ('top');
+insert into CategoryInfo(category_name) values ('bottom');
+insert into CategoryInfo(category_name) values ('shoes');
+insert into CategoryInfo(category_name) values ('accessory');
+insert into CategoryInfo(category_name) values ('scenery');
+insert into CategoryInfo(category_name) values ('mountain');
+insert into CategoryInfo(category_name) values ('ocean');
+insert into CategoryInfo(category_name) values ('people');
+insert into CategoryInfo(category_name) values ('food');
+insert into CategoryInfo(category_name) values ('meal');
+insert into CategoryInfo(category_name) values ('dessert');
+insert into CategoryInfo(category_name) values ('receipt');
+insert into CategoryInfo(category_name) values ('barcode');
+insert into CategoryInfo(category_name) values ('screenshot');
+insert into CategoryInfo(category_name) values ('vehicle');
+insert into CategoryInfo(category_name) values ('interior');
+
+insert into parent_category(category_name, parent_name) values ('dog', 'animal');
+insert into parent_category(category_name, parent_name) values ('cat', 'animal');
+insert into parent_category(category_name, parent_name) values ('fish', 'animal');
+insert into parent_category(category_name, parent_name) values ('hamster', 'animal');
+insert into parent_category(category_name, parent_name) values ('leaf', 'plant');
+insert into parent_category(category_name, parent_name) values ('flower', 'plant');
+insert into parent_category(category_name, parent_name) values ('tree', 'plant');
+insert into parent_category(category_name, parent_name) values ('top', 'fashion');
+insert into parent_category(category_name, parent_name) values ('bottom', 'fashion');
+insert into parent_category(category_name, parent_name) values ('shoes', 'fashion');
+insert into parent_category(category_name, parent_name) values ('accessory', 'fashion');
+insert into parent_category(category_name, parent_name) values ('mountain', 'scenery');
+insert into parent_category(category_name, parent_name) values ('ocean', 'scenery');
+insert into parent_category(category_name, parent_name) values ('meal', 'food');
+insert into parent_category(category_name, parent_name) values ('dessert', 'food');
