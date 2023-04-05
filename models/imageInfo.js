e66c397d476efcbe45e6053cb2b6315fb1c10a6d
zjaym1703 <jym971024@gmail.com>
2023-04-06 02:08:05 +0900

    [#25] search sql 문 수정

--- models/imageInfo.js
+++ models/imageInfo.js
@@ -1,21 +1,20 @@
 const ImageInfo = {
   // 이미지 정보 쿼리
   getImageInfoQuery: (uid) => `SELECT * FROM imageinfo where uid='${uid}'`,
-  getSearchResult :(word) => `select id,uid,image_url,image_date,image_location,image_width,image_height,b.category_name
+  getSearchResult :(word) => `select id,uid,image_url,image_date,image_location,image_width,image_height,b.category_name,parent_name
                               from ImageInfo a INNER JOIN ImageCategory b ON a.id = b.image_id 
-                              INNER JOIN parent_category c on b.category_name = c.category_name 
+                              LEFT OUTER JOIN parent_category c on b.category_name = c.category_name 
                               where image_location like '%${word}%' or b.category_name like '%${word}%' or parent_name like '%${word}%';`,
-  getSearchWordColorResult : (word,image_id) => `select id,uid,image_url,image_date,image_location,image_width,image_height,b.category_name,rgb_info
+  getSearchWordColorResult : (word,image_id) => `select id,uid,image_url,image_date,image_location,image_width,image_height,b.category_name,parent_name,rgb_info
                                                 from ImageInfo a INNER JOIN ImageCategory b ON a.id = b.image_id 
-                                                INNER JOIN parent_category c on b.category_name = c.category_name 
+                                                LEFT OUTER JOIN parent_category c on b.category_name = c.category_name 
                                                 INNER JOIN (
                                                 select image_id, json_arrayagg(json_object('red',r,'green',g,'blue',b,'type',rgb_type)) as rgb_info
                                                 from palette
                                                 group by palette.image_id) d on b.image_id = d.image_id
-                                                where image_location like '%${word}%' 
+                                                where b.image_id in(${image_id}) and (image_location like '%${word}%' 
                                                 or b.category_name like '%${word}%'
-                                                or parent_name like '%${word}%'
-                                                or d.image_id in(${image_id});`,
+                                                or parent_name like '%${word}%');`,
   getSimilarColors : (minR,maxR,minG,maxG,minB,maxB) => `SELECT image_id FROM palette WHERE r BETWEEN ${minR} AND ${maxR} AND g BETWEEN ${minG} AND ${maxG} AND b BETWEEN ${minB} AND ${maxB}`
 };
 
