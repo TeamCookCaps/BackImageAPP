5ffa2cdf41509623032d6cd83966c6de81b3269c
skc4234 <skc4234@naver.com>
2023-04-14 15:12:40 +0900

    [#32] 복구 기능 개발 완료

---
+++ api/routes/restoreAll.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { asyncErrorWrapper } from "../../asyncErrorWrapper.js";
+import removeImage from "../../models/removeImage.js";
+import { DeleteService } from "../../services/delete.js";
+
+const route = Router();
+
+export default (app) => {
+    app.use("/restoreAll", route);
+
+    route.post("/", asyncErrorWrapper(async (req, res, next) => {
+            try {
+                const { trashId } = req.body;
+                const deleteServiceInstance = new DeleteService({ removeImage });
+                const result = await deleteServiceInstance.restoreAllImage(trashId);
+                res.status(201).json({
+                    location: "success",
+                    msg: null,
+                    data: result,
+                });
+            } catch (error) {
+                res.status(400).json({
+                    location: "error",
+                    msg: "휴지통 전체 복구가 실패했습니다",
+                    data: String(error),
+                });
+            }
+        })
+    );
+};
--- services/delete.js
+++ services/delete.js
@@ -32,4 +32,18 @@ export class DeleteService {
             throw new Error(error);
         }
     }
+
+    // 휴지통 전부 복구
+    async restoreAllImage(trashId) {
+        const req_query = this.removeImage.restoreAllImage(trashId);
+        console.log(req_query);
+        try {
+            const connect = await pool.getConnection(async (conn) => conn);
+            const result = await connect.query(req_query);
+            connect.release();
+            return result;
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
 }
