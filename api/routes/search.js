e8caa1b5559ca1f165868a9aae2e624b4b6b48c0
zjaym1703 <jym971024@gmail.com>
2023-04-28 20:58:28 +0900

    [#43] 검색 시 사용자도 검색되도록 로직 수정

--- api/routes/search.js
+++ api/routes/search.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { SearchService } from '../../services/search.js';
+import { UserService } from '../../services/user.js';
 import ImageInfo from '../../models/imageInfo.js';
+import User from '../../models/user.js';
 import { asyncErrorWrapper } from '../../asyncErrorWrapper.js';
 
 const route = Router();
@@ -13,27 +15,31 @@ export default (app) => {
         try{
             const { uid, word, color } = req.query;
             console.log(uid, word, color)
+
+            //유저 정보 return
+            const UserServiceInstance = new UserService({User});
+            const userList = await UserServiceInstance.searchUserInfo(word);
+
             let SearchServiceInstance = new SearchService({ImageInfo});
-     
+            let searchList;
             if(color === undefined || color == ''){ //word search
-                const searchList = await SearchServiceInstance.searchWord(uid, word);
-                res.status(201).json(
-                    {
-                        location : 'success',
-                        msg : null,
-                        data : searchList,
-                    }
-                );
+                searchList = await SearchServiceInstance.searchWord(uid, word);
+                
             }else{ // word+color search
-                const searchWCResult = await SearchServiceInstance.searchWordColor(uid, word,color);
-                res.status(201).json(
-                    {
-                        location : 'success',
-                        msg : null,
-                        data : searchWCResult,
-                    }
-                );
+                searchList = await SearchServiceInstance.searchWordColor(uid, word,color);
             }
+            res.status(201).json(
+                {
+                    location : 'success',
+                    msg : null,
+                    data : 
+                    {
+                        user : userList,
+                        searchList : searchList,
+                    }
+                }
+            );
+
         }catch(error){
             res.status(500).json(
                 {
