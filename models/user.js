e904cceab75b79c628c8582dc289758855a012a5
zjaym1703 <jym971024@gmail.com>
2023-04-28 20:06:04 +0900

    [#46] user 정보 검색 쿼리 추가

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const User = {
     //회원가입 쿼리 
     registerQuery : (uuid, nick) => `insert into User values('${uuid}', '${nick}')`,
+    getUser : (nick) => `select * from User where nick_name="${nick}"`,
 }
 
 export default User;
